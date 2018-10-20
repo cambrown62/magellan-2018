bd7359e47abdc0d207c8fe2047db0df506c453eb
Andrew Lobos <andrew@lobos.me>
2018-10-20 13:48:52 -0400

    Velocity only

--- src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.cpp
+++ src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.cpp
@@ -29,15 +29,12 @@ static void SetupEncoderISR() {
 
 EncoderPublisher::EncoderPublisher(ros::NodeHandle& nh) :
         nh_(nh),
-        encoder_msg_(),
-        encoder_publisher_("/platform/encoders", &encoder_msg_),
         velocity_publisher_("/platform/velocity", &twist_msg_),
         update_rate_(ENCODER_UPDATE_HZ),
         last_left_count_(0),
         last_right_count_(0) {
     SetupEncoderISR();
 
-    nh.advertise(encoder_publisher_);
     nh.advertise(velocity_publisher_);
 
     twist_msg_.header.frame_id = "base_link";
@@ -46,7 +43,7 @@ EncoderPublisher::EncoderPublisher(ros::NodeHandle& nh) :
 
 void EncoderPublisher::Update(bool forward) {
     if ( update_rate_.NeedsRun() ) {
-        // Atomic region
+        // Update encoder counts
         cli();
         long int left_delta = left_encoder_count - last_left_count_;
         long int right_delta = right_encoder_count - last_right_count_;
@@ -55,21 +52,17 @@ void EncoderPublisher::Update(bool forward) {
         last_right_count_ = right_encoder_count;
         sei();
 
+        // Hack for counting in reverse without quad encoders
         if (!forward) {
             left_delta *= -1;
             right_delta *= -1;
         }
 
-        encoder_msg_.header.stamp = nh_.now();
-        encoder_msg_.left_delta = left_delta;
-        encoder_msg_.right_delta = right_delta;
-
+        // Compute and publish velocity in robot frame
         double avg_speed = ((left_delta + right_delta) / 2.0) * kDistancePerTick / (1.0 / ENCODER_UPDATE_HZ);
         twist_msg_.header.stamp = nh_.now();
         twist_msg_.twist.twist.linear.x = avg_speed;
 
         velocity_publisher_.publish(&twist_msg_);
-
-        encoder_publisher_.publish(&encoder_msg_);
     }
 }
--- src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
+++ src/magellan_firmware/firmware/magellan_controller/EncoderPublisher.h
@@ -2,7 +2,6 @@
 #define ENCODER_PUBLISHER_H
 
 #include <ros.h>
-#include <magellan_core/EncoderDeltaStamped.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
 #include "Rate.h"
 
@@ -12,8 +11,6 @@ public:
     void Update(bool forward);
 private:
     ros::NodeHandle& nh_;
-    magellan_core::EncoderDeltaStamped encoder_msg_;
-    ros::Publisher encoder_publisher_;
     geometry_msgs::TwistWithCovarianceStamped twist_msg_;
     ros::Publisher velocity_publisher_;
     Rate update_rate_;
