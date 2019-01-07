b7c75898cbe4748b2ca96a60ab73e4e52b47086d
Xinke Chen <xinkecf35@gmail.com>
2019-01-06 23:13:39 -0500

    Added some basic logic
    This commit just fleshes out a few more details, as of right now no ability to control rate of updates, will probably need Rate to included

--- src/magellan_sim/MagellanSim.cpp
+++ src/magellan_sim/MagellanSim.cpp
@@ -2,11 +2,61 @@
 
 MagellanSim::MagellanSim(ros::NodeHandle& nh) :
     node_handle_(nh),
-    commanded_throttle_(0.0),
-    throttle_subscriber_("/platform/cmd_velocity", &Robot::updateVelocity, this),
-    steering_subscriber_("/platform/cmd_turning_radius", &Robot::updateSteering, this),
-    commanded_steering_angle_(0.0),
+    commanded_velocity_(0.0),
+    commanded_radius_(0.0),
     velocity_(0.0),
     steering_angle_(0.0) {
-    
+    throttle_subscriber_ = 
+        node_handle_.subscribe("/platform/cmd_velocity", 1, &MagellanSim::UpdateThrottle, this);
+    steering_subscriber_ =
+        node_handle_.subscribe("platform/cmd_turning_radius", 1, &MagellanSim::UpdateSteering, this);
+    velocity_publisher_ =
+        node_handle_.advertise<geometry_msgs::TwistWithCovarianceStamped>("/platform/velocity", 1, true);
+    turning_radius_publisher_ =
+        node_handle_.advertise<std_msgs::Float64>("/platform/turning_radius", 1, true);
+    time_ = ros::Time::now();
+}
+
+void MagellanSim::Update() {
+    UpdateVelocity();
+    UpdateYaw();
+}
+
+void MagellanSim::UpdateVelocity() {
+    geometry_msgs::TwistWithCovarianceStamped twist_msg;
+    twist_msg.header.stamp = time_;
+    twist_msg.twist.twist.linear.x = velocity_;
+    velocity_publisher_.publish(&twist_msg);
+}
+
+void MagellanSim::UpdateYaw() {
+    std_msgs::Float64 course_msg;
+    course_msg.data = commanded_radius_;
+    turning_radius_publisher_.publish(&course_msg);
+}
+
+/*
+ * Updates commanded velocity from message and then updates sim's velocity
+ * This member function just calculates the ideal velocity 
+ * At this point, this member functions has no upper limit on the vehicle
+ */
+void MagellanSim::UpdateThrottle(const std_msgs::Float64& cmd_velocity) {
+    commanded_velocity_ = cmd_velocity.data;
+    // NOTE TO SELF: it probably just sufficient to match velocity and 
+    // not bother with the physics equations
+    ros::Time current_time = ros::Time::now();
+    double sim_accel = 3.5765; // acceleration, 0-40 in 5 seconds
+    if (commanded_velocity_ - velocity_ > 0.005) {
+        ros::Duration delta_time = current_time - time_;
+        velocity_ += sim_accel*delta_time.toSec();
+    }
+    time_ = current_time;
+}
+
+/*
+ * Updates commanded velocity from message and udpates sim turning cmd_turning_radius
+ * At this point the sim just immediately matches the commanded turning cmd_radius
+ */ 
+void MagellanSim::UpdateSteering(const std_msgs::Float64& cmd_radius) {
+    commanded_radius_ = cmd_radius.data;
 }
--- src/magellan_sim/MagellanSim.h
+++ src/magellan_sim/MagellanSim.h
@@ -2,30 +2,32 @@
 #define MAGELLANSIM_H_
 
 #include <ros.h>
+#include <time.h>
+#include <duration.h>
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/TwistWithCovarianceStamped.h>
 
 class MagellanSim {
 public:
     MagellanSim(ros::NodeHandle& nh);
-    void updateVelocity();
-    void updateYaw();
-    void update();
+    void UpdateVelocity();
+    void UpdateYaw();
+    void Update();
 
 private:
+    ros::Time time_;
     ros::NodeHandle& node_handle_;
-    ros::Subscriber<std_msgs::Float64, MagellanSim> throttle_subscriber_;
-    ros::Subscriber<std_msgs::Float64, MagellanSim> steering_subscriber_;
+    ros::Subscriber throttle_subscriber_;
+    ros::Subscriber steering_subscriber_;
     ros::Publisher velocity_publisher_;
     ros::Publisher turning_radius_publisher_;
-    std_msgs::Float64 turning_radius_msg_;
-    geometry_msgs::TwistWithCovarianceStamped twist_msgs_;
-    double commanded_throttle_;
-    double commanded_steering_angle_;
+    double commanded_velocity_;
+    double commanded_radius_;
     double velocity_;
     double steering_angle_;
-    void updateThrottle(const std_msgs::Float64& cmd_velocity);
-    void updateSteering(const std_msgs::Float64& cmd_radius);
+
+    void UpdateThrottle(const std_msgs::Float64& cmd_velocity);
+    void UpdateSteering(const std_msgs::Float64& cmd_radius);
 };
 
 #endif
