31e85a8d3f6170f40612ac45efbe8342b4b7cc9e
Kaylene <viola.sox@gmail.com>
2019-01-20 17:08:32 -0500

    Example images for targets

--- src/magellan_vision/scripts/detect_targets.py
+++ src/magellan_vision/scripts/detect_targets.py
@@ -1,33 +1,45 @@
 import cv2 as cv
+import numpy as np
+
+# Temporary for testing - hues from the images in rules document
+hueArch = 39
+huePole = 255
+hueRamp = 156
 
 # These should probably be ROSparams
-hueMinArch = 0
-hueMaxArch = 100
-hueMinRamp = 0
-hueMaxRamp = 100
-hueMinPole = 0
-hueMaxPole = 100 
+hueMinArch = hueArch - 20
+hueMaxArch = hueArch + 20
+hueMinRamp = hueRamp - 20
+hueMaxRamp = hueRamp + 20
+hueMinPole = huePole - 20
+hueMaxPole = huePole + 20
 cannyThresh = 100
 # I think these should be the same for all targets
+# Sat should be wide for various outdoors conditions
 satMin = 0
-satMax = 0
+satMax = 255
+# Low val = less color - raise min to reduce risk of picking up non-target objects
 valMin = 0
-valMax = 0
+valMax = 255
 
 # Get the current frame from ROS
-frame = None
+#frame = None
+frame = cv.imread('Hoop.png', cv.IMREAD_COLOR)
 
 frameHSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 archThreshold = cv.inRange(frameHSV, (hueMinArch, satMin, valMin), (hueMaxArch, satMax, valMax))
+cv.imshow('image', archThreshold)
+key = cv.waitKey(0)
+
 
 # Arch detection should look for two vertical lines and find the midpoint
 
-rampThreshold = cv.inRange(frameHSV, (hueMinRamp, satMin, valMin), (hueMaxRamp, satMax, valMax))
-rampBlur = cv.blur(rampThreshold, (3, 3))
-rampEdges = cv.canny(rampBlur, cannyThresh, cannyThresh*2)
-
-# Ramp detection should find the center of the blob
-
-poleThreshold = cv.inRange(frameHSV, (hueMinPole, satMin, valMin), (hueMaxPole, satMax, valMax))
+# rampThreshold = cv.inRange(frameHSV, (hueMinRamp, satMin, valMin), (hueMaxRamp, satMax, valMax))
+# rampBlur = cv.blur(rampThreshold, (3, 3))
+# rampEdges = cv.canny(rampBlur, cannyThresh, cannyThresh*2)
+# 
+# # Ramp detection should find the center of the blob
+# 
+# poleThreshold = cv.inRange(frameHSV, (hueMinPole, satMin, valMin), (hueMaxPole, satMax, valMax))
 
 # Pole detection should find the center of the blob
